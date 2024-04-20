c9263a34a64caba081c5e710372e4973537322e8
a.frolov <a.frolov@bi.zone>
2024-04-20 17:28:58 +0300

    initial commit

---
+++ lesson_3/main.py
@@ -0,0 +1,2 @@
+def solve(a: float, b: float, c: float) -> tuple[float, float]:
+    return (0, 0)
---
+++ pytest/test_solve.py
@@ -0,0 +1,5 @@
+from lesson_3.main import solve
+
+
+def test_solve():
+    assert solve(1, 1) == 0, 0
